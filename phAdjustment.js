7e0f1dd14b5a2cdb83e25be18e7bf6c2fa379710
GetOvert-LabResults <info.getovert@gmail.com>
2026-03-04 12:16:21 -0600

    Add files via upload

---
+++ phAdjustment.js
@@ -0,0 +1,17 @@
+const btnEl = document.getElementById("btn");
+const lacticInputEl = document.getElementById("lactic-result");
+
+function calculateAddition() {
+  const currentPhValue = document.getElementById("currentPh").value;
+  const targetPhValue = document.getElementById("targetPh").value;
+  const grainKgValue = document.getElementById("grainLbs").value * 0.45359237;
+  const molarContentValue = document.getElementById("molarContent").value;
+  // protons required to move a wort made with 1 kg of malt 1.0 pH
+  const protons = 30.262;
+  const pHdiff = currentPhValue - targetPhValue;
+  const mLValue = (grainKgValue * protons) * pHdiff / molarContentValue;
+  const roundedValue = Math.round(mLValue / 5) * 5;
+
+  lacticInputEl.value = roundedValue;
+}
+btnEl.addEventListener("click", calculateAddition);
